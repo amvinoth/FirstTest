3fed2fabdb67b795e46289c7cdd78fc61a5b6bba
amvinoth <amvinothkumar6@gmail.com>
2020-03-20 22:37:05 +0530

    My new component for reactive forms

---
+++ TestFolder/HelloAngular/src/app/rform/rform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rform',
+  templateUrl: './rform.component.html',
+  styleUrls: ['./rform.component.css']
+})
+export class RFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
